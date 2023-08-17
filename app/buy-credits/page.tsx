217520db889601214c521c0dcf75c0e553a6d791
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-08-18 00:41:23 +0100

    feat: Add pricing page

--- app/buy-credits/page.tsx
+++ app/buy-credits/page.tsx
@@ -18,13 +18,68 @@ function BuyCredit() {
           </div>
         </div>
         <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-500 sm:mb-10">
-          You have <span className="font-semibold text-gray-400">1 credit</span>
-          . Join thousands of happy customers by buying more below.
+          You have{" "}
+          <span className="font-semibold text-gray-400">unlimited credit</span>.
+          Join thousands of happy customers by buying more below.
         </p>
       </main>
+
+      <div className="p-4 flex flex-col lg:flex-row gap-6">
+        <div className="flex flex-col p-6">
+          <p className=" text-xl font-semibold py-1">30 Credits</p>
+          <p className=" text-gray-400">30 room redesigns with any theme.</p>
+          <p className=" text-4xl font-bold py-6">$9</p>
+          <button
+            className=" rounded-md p-3  Button--lg"
+            type="button"
+            style={{
+              backgroundColor:
+                "rgb(52, 130, 246); border-color: rgb(52, 130, 246",
+            }}
+          >
+            <div className="flex-container justify-content-center align-items-center">
+              <span className="text-white font-medium ">Pay</span>
+            </div>
+          </button>
+        </div>
+        <div className="flex flex-col border border-gray-400 bg-[#2f3033] rounded-lg p-6">
+          <p className=" text-xl font-semibold py-1">100 Credits</p>
+          <p className=" text-gray-400">100 room redesigns with any theme.</p>
+          <p className=" text-4xl font-bold py-6">$19</p>
+          <button
+            className=" rounded-md p-3  Button--lg"
+            type="button"
+            style={{
+              backgroundColor:
+                "rgb(52, 130, 246); border-color: rgb(52, 130, 246",
+            }}
+          >
+            <div className="flex-container justify-content-center align-items-center">
+              <span className="text-white font-medium ">Pay</span>
+            </div>
+          </button>
+        </div>
+        <div className="flex flex-col p-6">
+          <p className=" text-xl font-semibold py-1">200 Credits</p>
+          <p className=" text-gray-400">200 room redesigns with any theme.</p>
+          <p className=" text-4xl font-bold py-6">$29</p>
+          <button
+            className=" rounded-md p-3  Button--lg"
+            type="button"
+            style={{
+              backgroundColor:
+                "rgb(52, 130, 246); border-color: rgb(52, 130, 246",
+            }}
+          >
+            <div className="flex-container justify-content-center align-items-center">
+              <span className="text-white font-medium ">Pay</span>
+            </div>
+          </button>
+        </div>
+      </div>
       <div className="mt-10 text-center">
         <h4 className="flex-none leading-6 mt-2 text-2xl font-bold tracking-tight text-white sm:text-4xl">
-          What’s included
+          What&apos;s included
         </h4>
       </div>
 
